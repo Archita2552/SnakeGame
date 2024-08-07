17d6494dbf811d0c0e8ef1d2674569fdb54e1ef5
Archita Jain <122081789+Archita2552@users.noreply.github.com>
2024-08-07 12:07:44 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -4,18 +4,29 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        self.high_score=0
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}",False,"center",("Arial", 15, "normal"))
+        # self.write(f"Score: {self.score}",False,"center",("Arial", 15, "normal"))
         self.hideturtle()
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}" , False, "center", ("Arial", 15, "normal"))
+         
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score=self.score
+            self.score=0
+            self.update_scoreboard()
 
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over",False,"Center",("Arial", 15, "normal"))
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game Over",False,"Center",("Arial", 15, "normal"))
 
     def increase_score(self):
         self.score +=1
-        self.clear()
-        self.write(f"Score: {self.score}", False, "center", ("Courier", 15, "normal"))
+        # self.clear()
+        # self.write(f"Score: {self.score}", False, "center", ("Courier", 15, "normal"))
+        self.update_scoreboard()
